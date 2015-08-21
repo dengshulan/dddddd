3b4ed2f66e8aaeffd84e3d319a69998e46655131
student <student@ranyu.local>
2015-08-21 14:55:46 +0800

    Initial Commit

---
+++ dddddd/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  dddddd
+//
+//  Created by student on 15/8/21.
+//  Copyright (c) 2015年 student. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
